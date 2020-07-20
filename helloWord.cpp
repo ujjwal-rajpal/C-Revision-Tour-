51f81c4cbbc7e5b139d2247762235f1d45274dba
Ujjwal Rajpal <ujjwalr@mindfiresolutions.com>
2020-07-20 15:01:18 +0530

    Initial commit with Hello Word program

---
+++ helloWord.cpp
@@ -0,0 +1,19 @@
+
+/**	
+  * print program helo wrld in c++
+  *	 purpose to get started with c++(revision tour)s
+  *written by : ujjwal
+  *
+*/
+
+
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+	
+	cout << "Hello World";
+	return 0;
+}
